d9f997c86f2d0a290591d1a1c7b7136a532890db
Mezdelex <alexcondegomez@gmail.com>
2022-12-04 16:46:57 +0100

    Changed Part2 to inverse logic for less LOC

--- day04/Day04.cs
+++ day04/Day04.cs
@@ -9,7 +9,7 @@ public record struct Day04
     {
         string input = await File.ReadAllTextAsync("../../../day04/input.txt");
 
-        return input.Split(Utils.NEW_LINE).Select(chunk => new Regex("[,-]").Split(chunk));
+        return input.Trim().Split(Utils.NEW_LINE).Select(chunk => new Regex("[,-]").Split(chunk));
     }
 
     public static async Task<int> Part1() => (await ProcessInput())
@@ -19,11 +19,8 @@ public record struct Day04
         .Sum();
 
     public static async Task<int> Part2() => (await ProcessInput())
-        .Select(arr => (int.Parse(arr[0]) <= int.Parse(arr[2]) && int.Parse(arr[1]) >= int.Parse(arr[2]))
-            | (int.Parse(arr[2]) <= int.Parse(arr[0]) && int.Parse(arr[3]) >= int.Parse(arr[0]))
-            | (int.Parse(arr[1]) >= int.Parse(arr[3]) && int.Parse(arr[0]) <= int.Parse(arr[3]))
-            | (int.Parse(arr[3]) >= int.Parse(arr[1]) && int.Parse(arr[2]) <= int.Parse(arr[1]))
-            ? 1 : 0)
+        .Select(arr => int.Parse(arr[0]) > int.Parse(arr[3]) | int.Parse(arr[1]) < int.Parse(arr[2])
+            ? 0 : 1)
         .Sum();
 }
 
